5fb834f280c8a2f00354ab23df1c3d68130589b7
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-01 03:11:59 +0300

    Update Fetching Public Github Repos with axios

--- src/Hooks/GithubRepos.js
+++ src/Hooks/GithubRepos.js
@@ -1,14 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function GithubRepos() {
   const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/octocat/repos")
+//       .then((res) => res.json())
+//       .then((data) => setRepos(data))
+//       .catch((err) => console.error(err));
+//   }, []);
+
 
   useEffect(() => {
-    fetch("https://api.github.com/users/octocat/repos")
-      .then((res) => res.json())
-      .then((data) => setRepos(data))
-      .catch((err) => console.error(err));
-  }, []);
+    axios.get("https://api.github.com/users/octocat/repos")
+      .then((response) => {
+        setRepos(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError("Failed to fetch data");
+        setLoading(false);
+      });
+  }, []);  
+
+  if(loading){
+    return (
+        <h2>Loading Repositories...</h2>
+    )
+  }
+  if(error){
+    return (
+        <h2>{error}</h2>
+    )
+  }
+
 
   return (
     <div>
